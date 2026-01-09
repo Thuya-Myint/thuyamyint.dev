0f76a0f9e612056c25375209191480e669b851cf
thuyamyint <thuyamyint2022@gmail.com>
2026-01-10 01:10:48 +0900

    fix scroll type

--- interfaces/appContext.ts
+++ interfaces/appContext.ts
@@ -1,11 +1,10 @@
 import { Locale } from "@/types/language";
-import { Theme } from "@/types/theme";
 
 export interface AppContextType {
-    locale: Locale;
-    setLocale: (l: Locale) => void;
-    t: (key: string) => string;
-    toggleLocale: () => void
+  locale: Locale;
+  setLocale: (l: Locale) => void;
+  t: (key: string) => string;
+  toggleLocale: () => void
 
 
 }

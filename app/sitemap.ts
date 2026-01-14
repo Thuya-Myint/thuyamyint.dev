1c43426e8b2d45a3699a3b5836ccb2387a72a814
thuyamyint <thuyamyint2022@gmail.com>
2026-01-14 22:34:42 +0900

    add meta data and site map

---
+++ app/sitemap.ts
@@ -0,0 +1,39 @@
+import { MetadataRoute } from 'next';
+
+const BASE_URL = 'https://thuyamyint.dev';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${BASE_URL}/`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/journey`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${BASE_URL}/hire`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${BASE_URL}/contact`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+  ];
+
+}

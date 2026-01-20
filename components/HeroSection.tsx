7262e668e1116c9612af9ea578137b969718bb2d
thuyamyint <thuyamyint2022@gmail.com>
2026-01-21 01:06:55 +0900

    add margin to herosection

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ import PipelineShowcase from "@/components/PipelineShowCase";
 
 export default function HeroSection() {
   return (
-    <div className="relative z-10 grid  lg:grid-cols-2 gap-10 grid-cols-1 place-items-center  h-full">
+    <div className="relative z-10 grid mt-20 mb-10  lg:grid-cols-2 gap-10 grid-cols-1 place-items-center  h-full">
       <div className="order-2 lg:order-1 w-full">
         <PipelineShowcase />
       </div>
